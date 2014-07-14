05b9878aa0b43afcd576aeb005bcdec6ae62d986
Philip Bauer <bauer@starzel.de>
2014-07-14 10:55:24 +0200

    do not remove existing content on reinstall

--- Products/Doormat/Extensions/Install.py
+++ Products/Doormat/Extensions/Install.py
@@ -1,7 +1,9 @@
 from Products.CMFCore.utils import getToolByName
 
 
-def uninstall(portal):
+def uninstall(portal, reinstall=False):
+    if reinstall:
+        return
     setup_tool = getToolByName(portal, 'portal_setup')
     setup_tool.runAllImportStepsFromProfile(
         'profile-Products.Doormat:uninstall')
